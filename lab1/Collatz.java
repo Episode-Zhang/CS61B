1d3404dcb246b4ce98ae1aa64fa13349dbde1fa2
Episode-Z <shoinchifu@gmail.com>
2022-03-16 16:57:19 +0800

    submission of lab1

---
+++ lab1/Collatz.java
@@ -0,0 +1,28 @@
+/** Class that prints the Collatz sequence starting from a given number.
+ *  @author Episode Zhang
+ */
+public class Collatz {
+
+    /** Given a particular number, the method will return a new number according to Collatz's rule.
+     *  @param n: The given number that will be transformed by Collatz's rule
+     */
+    public static int nextNumber(int n) {
+        if (n % 2 == 0) {
+            return n / 2;
+        }
+        else {
+            return 3 * n + 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.print(n + " ");
+        while (n != 1) {
+            n = nextNumber(n);
+            System.out.print(n + " ");
+        }
+        System.out.println();
+    }
+}
+
