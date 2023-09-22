0df0649ecbda53a5ed52354e7b216254aa4d747b
JefferyZhang <zhanning12@gmail.com>
2023-09-22 23:25:37 +0800

    Add a add method.

--- proj2/gitlet/StagedArea.java
+++ proj2/gitlet/StagedArea.java
@@ -1,7 +1,7 @@
 package gitlet;
 
 import java.io.File;
-import java.util.TreeMap;
+import java.util.HashMap;
 
 /**
  * Staged area stores added files with their blob between each two commits.
@@ -9,16 +9,30 @@ import java.util.TreeMap;
  */
 public class StagedArea {
 
-    private TreeMap<File, Blob> tree;
+    /** Maintains a file-name-to-blob map.  */
+    private HashMap<File, Blob> fileStorage;
 
+    /** This class uses singleton pattern. */
     private static StagedArea instance = null;
 
+    /** Constructor. */
     private StagedArea() {}
 
+    /** The initialization of the unique instance will be delayed to the first time it is used. */
     public static StagedArea getInstance() {
         if (instance == null) {
             instance = new StagedArea();
         }
         return instance;
+    }
+
+    /**
+     * Add a file to the staged area.
+     *
+     * @param filename The name of the file to be added.
+     * @param blob The blob corresponding to the given filename.
+     */
+    public void add(String filename, Blob blob) {
+        instance.add(filename, blob);
     }
 }
