a7388dca9a83f9f40b742c9e26f393896abacc65
JefferyZhang <zhanning12@gmail.com>
2023-09-28 00:13:16 +0800

    Add a Global class to represent the global area.

---
+++ proj2/gitlet/Global.java
@@ -0,0 +1,11 @@
+package gitlet;
+
+
+/** This class stores global object (and almost they are all unique). */
+public class Global {
+
+    // TODO use observer pattern. HEAD always points to the latest commit node.
+    private static CommitNode HEAD;
+
+
+}
