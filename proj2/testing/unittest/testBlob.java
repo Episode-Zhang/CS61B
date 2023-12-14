c41036de6d545b2b4f446ab7c10421fa394e9b69
JefferyZhang <zhanning12@gmail.com>
2023-12-14 18:14:37 +0800

    Updated and added new tests.

--- proj2/testing/unittest/testBlob.java
+++ proj2/testing/unittest/testBlob.java
@@ -8,25 +8,35 @@ import org.junit.Test;
 public class testBlob {
 
     @Test
-    public void testNormalAdd1() throws Helper.FileDoesNotExistException {
+    public void testSaveWithoutCwdMark() throws Helper.FileDoesNotExistException {
         Blob blob = Blob.createBlob("testRepo/Test.java");
+        // should print file contents
         Helper.printBlobContent(blob);
     }
 
     @Test
-    public void testNormalAdd2() throws Helper.FileDoesNotExistException {
+    public void testSaveWithCwdMark() throws Helper.FileDoesNotExistException {
         Blob blob = Blob.createBlob("./testRepo/Test.java");
+        // should print file contents
+        Helper.printBlobContent(blob);
+    }
+
+    @Test(expected = Helper.FileDoesNotExistException.class)
+    public void testSaveWithNonExistingFile() throws Helper.FileDoesNotExistException {
+        Blob blob = Blob.createBlob("testRepo/Tast.java");
+        // should raise an exception
         Helper.printBlobContent(blob);
     }
 
     @Test
-    public void testErrorAdd1() {
-        try {
-            Blob blob = Blob.createBlob("testRepo/Tast.java");
-            Helper.printBlobContent(blob);
-        } catch (Helper.FileDoesNotExistException e) {
-            System.out.println("File does not exist.");
-            System.exit(0);
-        }
+    public void testReadWithoutCwdMark() {
+        // should print file contents
+        Helper.printBlobContent(".gitlet/blobs/18/3ba7a5395fb4dd0a1062fb04164577d7654b73");
+    }
+
+    @Test
+    public void testReadWithCwdMark() {
+        // should print file contents
+        Helper.printBlobContent("./.gitlet/blobs/18/3ba7a5395fb4dd0a1062fb04164577d7654b73");
     }
 }
--- proj2/testing/unittest/testStagingArea.java
+++ proj2/testing/unittest/testStagingArea.java
@@ -8,16 +8,39 @@ import org.junit.Test;
 public class testStagingArea {
 
     @Test
-    public void testNormalAdd1() throws Helper.FileDoesNotExistException {
-        final StagingArea stagingArea = StagingArea.getInstance();
-        final String filePath = "testRepo/Test.java";
+    public void testAddWithoutCwdMark() throws Helper.FileDoesNotExistException {
+        StagingArea stagingArea = StagingArea.getInstance();
+        String filePath = "testRepo/Test.java";
         stagingArea.add(filePath);
     }
 
     @Test
-    public void testNormalAdd2() throws Helper.FileDoesNotExistException {
-        final StagingArea stagingArea = StagingArea.getInstance();
-        final String filePath = "./testRepo/Test.java";
+    public void testAddWithCwdMark() throws Helper.FileDoesNotExistException {
+        StagingArea stagingArea = StagingArea.getInstance();
+        String filePath = "./testRepo/Test.java";
+        stagingArea.add(filePath);
+    }
+
+    @Test
+    public void testReadStagingFiles() {
+        StagingArea stagingArea = StagingArea.getInstance();
+        // should print name of files in staging area.
+        System.out.println(stagingArea.getStagingFileName());
+    }
+
+    @Test
+    public void testAddMultipleFiles() throws Helper.FileDoesNotExistException {
+        StagingArea stagingArea = StagingArea.getInstance();
+        String filePath1 = "./testRepo/Test.java";
+        String filePath2 = "./testRepo/hi.txt";
+        stagingArea.add(filePath1);
+        stagingArea.add(filePath2);
+    }
+
+    @Test(expected = Helper.FileDoesNotExistException.class)
+    public void testAddNonExistingFile() throws Helper.FileDoesNotExistException {
+        StagingArea stagingArea = StagingArea.getInstance();
+        String filePath = "./testRepo/Tast.java";
         stagingArea.add(filePath);
     }
 }
