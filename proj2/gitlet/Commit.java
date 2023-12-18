ea63571b6daec761258de3bdd7c665ec33e2081d
JefferyZhang <zhanning12@gmail.com>
2023-12-19 01:22:28 +0800

    Finish Commit's init and createCommit methods with test passed.

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.TimeZone;
 import java.util.TreeMap;
 import java.text.SimpleDateFormat;
 
@@ -61,40 +62,56 @@ public class Commit implements Serializable {
      * @return a Commit instance.
      */
     public static Commit createCommit(Commit parent, String message, StagingArea stagingArea) {
-        // create parent of the commit instance
+        // common variables for both initial commit and non-initial commit
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        final int DIR_BOUND = 2;
+        final String COMMIT_ROOTDIR = "/commits/";
+        // create the initial commit
+        if (stagingArea == null) {
+            // id
+            final String INIT_ID = "-1";
+            String sha1_id = Utils.sha1(INIT_ID);
+            // timestamp
+            String INIT_TIMEZONE = "UTC";
+            formatter.setTimeZone(TimeZone.getTimeZone(INIT_TIMEZONE));
+            String timestamp = formatter.format(new Date(0));
+            // dest
+            String commitDir = COMMIT_ROOTDIR;
+            String commitName = "root";
+            File commitDest = Utils.join(Helper.ROOT_DIR, Helper.REPO_DIR, commitDir);
+            // create init commit
+            Commit commit = new Commit(sha1_id, null, null, timestamp, message, null, commitDest);
+            commit.save(commitName);
+            return commit;
+        }
+        // create a non-initial commit
+        // parent
         ArrayList<Commit> parents = new ArrayList<>();
         parents.add(parent);
-        // create timestamp of the commit instance
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        // timestamp
         String timestamp = formatter.format(new Date());
-        // create file-blob map for this commit
+        // file-blob table
         TreeMap<File, Blob> fileBlobTable = stagingArea.moveTable();
-        // create id for this commit
+        // id
         StringBuilder blobIds = new StringBuilder();
         for (Blob blob : fileBlobTable.values()) {
             blobIds.append(blob.getId());
         }
         String commitId = Utils.sha1(blobIds.toString());
-        // create saving destination for this commit
-        int dirBound = 2;
-        String commitDir = "commits/" + commitId.substring(0, dirBound);
-        String commitName = commitId.substring(dirBound);
+        // saving destination
+        String commitDir = COMMIT_ROOTDIR + commitId.substring(0, DIR_BOUND);
+        String commitName = commitId.substring(DIR_BOUND);
         File commitDest = Utils.join(Helper.ROOT_DIR, Helper.REPO_DIR, commitDir);
         // create a new commit
         Commit commit = new Commit(commitId, null, parents, timestamp, message, fileBlobTable, commitDest);
+        parent.setChild(commit);
         commit.save(commitName);
         return commit;
     }
 
     /** Factory method to create an initial commit object. */
     public static Commit init() {
-        Commit commit = new Commit(
-                "-1", null, null,
-                "1970-01-01 00:00:00", "initial commit", null,
-                Utils.join(Helper.ROOT_DIR, Helper.REPO_DIR, "commits"));
-        commit.id = Utils.sha1(commit.id);
-        commit.save("root");
-        return commit;
+        return createCommit(null, "initial commit", null);
     }
 
     /** Using the commit message, timestamp, files to represent a commit object. */
@@ -103,7 +120,7 @@ public class Commit implements Serializable {
         ArrayList<String> files = new ArrayList<>();
         if (fileBlobTable != null) {
             for (File file : fileBlobTable.keySet()) {
-                files.add(file.toString());
+                files.add(file.toString().replace(Helper.ROOT_DIR, ""));
             }
         }
         return String.format("commit message: %s\n" +
@@ -111,6 +128,11 @@ public class Commit implements Serializable {
                 "commit files: %s\n",
                 message, timestamp, String.join(", ", files)
         );
+    }
+
+    /** Set child commit to this corresponding to the given commit. */
+    private void setChild(Commit childCommit) {
+        child = childCommit;
     }
 
     /**
